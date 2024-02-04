d7950d6d298b067968aa1dfe983907fcd5fc0e4f
LuisGPorto1995 <luisgporto3103@gmail.com>
2024-02-04 18:48:54 -0300

    message.php file is fixed, now it works as it should

--- message.php
+++ message.php
@@ -1,26 +1,26 @@
 <?php
-    //let's get all form values
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $website = $_POST['website'];
-    $message = $_POST['message'];
+    // Let's get all form values
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $website = isset($_POST['website']) ? $_POST['website'] : '';
+    $message = isset($_POST['message']) ? $_POST['message'] : '';
 
-    if(!empty($email) && !empty($message)) { //if email and message field is not empty
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) { // if the user entered a valid email
-            $receiver = "luisgporto3103@gmail.com"; //email receiver email address
-            $subject = "From: $name <$email>"; //subject of the email. Subject looks like From: luis <luisgporto3103@hmail.com>
+    if (!empty($email) && !empty($message)) { // If email and message field is not empty
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // If the user entered a valid email
+            $receiver = "luisgporto3103@gmail.com"; // Email receiver email address
+            $subject = "From: $name <$email>"; // Subject of the email. Subject looks like From: luis <luisgporto3103@hmail.com>
             $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message";
             $sender = "From: $email";
-            if(mail($receiver, $subject, $body, $sender)) {
+            if (mail($receiver, $subject, $body, $sender)) {
                 echo  "Your message has been sent!";
             } else {
                 echo "Sorry, failed to send your message!";
             }
-        }else {
+        } else {
             echo "Enter a valid email address!";
         }
-    }else {
+    } else {
         echo "Email and message required!";
     }
 ?>
