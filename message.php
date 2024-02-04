fa05a5058f1c498b81c5bbd91c9beb384e1c73d3
LuisGPorto1995 <luisgporto3103@gmail.com>
2024-02-04 19:58:58 -0300

    message.php updated and now it works even if there is an error sending the email

--- message.php
+++ message.php
@@ -1,26 +1,27 @@
 <?php
-    // Let's get all form values
-    $name = isset($_POST['name']) ? $_POST['name'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
-    $website = isset($_POST['website']) ? $_POST['website'] : '';
-    $message = isset($_POST['message']) ? $_POST['message'] : '';
+  // Let's get all form values
+  $name = isset($_POST['name']) ? $_POST['name'] : '';
+  $email = isset($_POST['email']) ? $_POST['email'] : '';
+  $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+  $website = isset($_POST['website']) ? $_POST['website'] : '';
+  $message = isset($_POST['message']) ? $_POST['message'] : '';
 
-    if (!empty($email) && !empty($message)) { // If email and message field is not empty
-        if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // If the user entered a valid email
-            $receiver = "luisgporto3103@gmail.com"; // Email receiver email address
-            $subject = "From: $name <$email>"; // Subject of the email. Subject looks like From: luis <luisgporto3103@hmail.com>
-            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message";
-            $sender = "From: $email";
-            if (mail($receiver, $subject, $body, $sender)) {
-                echo  "Your message has been sent!";
-            } else {
-                echo "Sorry, failed to send your message!";
-            }
-        } else {
-            echo "Enter a valid email address!";
-        }
-    } else {
-        echo "Email and message required!";
-    }
+  if (!empty($email) && !empty($message)) { // If email and message field is not empty
+      if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // If the user entered a valid email
+          $receiver = "luisgporto3103@gmail.com"; // Email receiver email address
+          $subject = "From: $name <$email>"; // Subject of the email. Subject looks like From: luis <luisgporto3103@hmail.com>
+          $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message";
+          $sender = "From: $email";
+
+          if (@mail($receiver, $subject, $body, $sender)) { // Suppress warnings with @
+              echo "Your message has been sent!";
+          } else {
+              echo "Sorry, failed to send your message!";
+          }
+      } else {
+          echo "Enter a valid email address!";
+      }
+  } else {
+      echo "Email and message required!";
+  }
 ?>
